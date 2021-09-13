99965082787ee0eb58133023b65e9fce92d0f837
Hoang Mai <hoangmai2139@gmail.com>
2021-09-13 19:59:44 +1000

    Add project files.

---
+++ Project1/Project1.cpp
@@ -0,0 +1,12 @@
+﻿// Project1.cpp : Defines the entry point for the application.
+//
+
+#include "Project1.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
